98b172483358c1e921b0a5000ec72cf43f8298fa
sonvuuu28 <vuthienson280104@gmail.com>
2024-10-08 14:10:38 +0700

    java

--- src/GUI/TrangChuGUI.java
+++ src/GUI/TrangChuGUI.java
@@ -5,9 +5,9 @@ import java.awt.Color;
 /**
  * @author SonVuuu
  */
-public class TrangChu extends javax.swing.JFrame {
+public class TrangChuGUI extends javax.swing.JFrame {
 
-    public TrangChu() {
+    public TrangChuGUI() {
         initComponents();
     }
 
@@ -524,20 +524,21 @@ public class TrangChu extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TrangChu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TrangChuGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TrangChu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TrangChuGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TrangChu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TrangChuGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TrangChu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TrangChuGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                TrangChu trang_mau = new TrangChu();
+                TrangChuGUI trang_mau = new TrangChuGUI();
                 trang_mau.setVisible(true);
                 trang_mau.setLocationRelativeTo(null);
                 
--- src/GUI/TrangLoginGUI.java
+++ src/GUI/TrangLoginGUI.java
@@ -8,12 +8,12 @@ package GUI;
  *
  * @author ACER
  */
-public class TrangLogin extends javax.swing.JFrame {
+public class TrangLoginGUI extends javax.swing.JFrame {
 
     /**
      * Creates new form TrangLogin
      */
-    public TrangLogin() {
+    public TrangLoginGUI() {
         initComponents();
     }
 
@@ -59,20 +59,21 @@ public class TrangLogin extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TrangLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TrangLoginGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TrangLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TrangLoginGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TrangLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TrangLoginGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TrangLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TrangLoginGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TrangLogin().setVisible(true);
+                new TrangLoginGUI().setVisible(true);
             }
         });
     }
