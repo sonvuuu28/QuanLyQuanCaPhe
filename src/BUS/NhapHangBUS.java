b0e0b9476da2fcb845add11b3a17c79d558385d8
DinhThai04 <149181200+DinhThai04@users.noreply.github.com>
2024-11-24 00:34:14 +0700

    Update NhapHangBUS.java

--- src/BUS/NhapHangBUS.java
+++ src/BUS/NhapHangBUS.java
@@ -4,73 +4,10 @@ import javax.swing.table.DefaultTableModel;
 
 
 import DAO.NhapHangDAO;
-import Util.JDBCUtil;
 
-import javax.swing.JOptionPane;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
-import java.sql.Statement;
 
-// public class NhapHangBUS {
-//     private NhapHangDAO nhapHangDAO;
-
-//     public NhapHangBUS() {
-//         nhapHangDAO = new NhapHangDAO();
-//     }
-
-//     public void loadDataToTable_KhoHang(JTable table) {
-//         ArrayList<Object[]> data = nhapHangDAO.getDataFromDatabase();
-//         DefaultTableModel model = (DefaultTableModel) table.getModel();
-//         model.setRowCount(0); // Clear existing data
-
-//         for (Object[] row : data) {
-//             String maNguyenLieu = (String) row[0];
-//             String tenNguyenLieu = (String) row[1];
-//             double khoiLuong = (double) row[2];
-//             String trangThai;
-
-//             if (khoiLuong >= 1) {
-//                 trangThai = "Còn hàng";
-//             } else if (khoiLuong > 0.1) {
-//                 trangThai = "Sắp hết";
-//             } else {
-//                 trangThai = "Hết hàng";
-//             }
-
-//             model.addRow(new Object[]{maNguyenLieu, tenNguyenLieu, khoiLuong, trangThai});
-//         }
-//     }
-    // public boolean isNguyenLieuInChoNhap(JTable table, String maNguyenLieu) {
-    //     DefaultTableModel model = (DefaultTableModel) table.getModel();
-    //     for (int i = 0; i < model.getRowCount(); i++) {
-    //         Object value = model.getValueAt(i, 0);
-    //         if (value != null && value.equals(maNguyenLieu)) {
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
-
-    // public static class NonEditableTableModel extends DefaultTableModel {
-    //     public NonEditableTableModel(Object[][] data, Object[] columnNames) {
-    //         super(data, columnNames);
-    //     }
-
-    //     @Override
-    //     public boolean isCellEditable(int row, int column) {
-    //         return false; // Tất cả các ô đều không thể chỉnh sửa
-    //     }
-    // }
-// }
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
-import java.util.ArrayList;
 
 public class NhapHangBUS {
     private NhapHangDAO nhapHangDAO;
@@ -127,23 +64,21 @@ public class NhapHangBUS {
         return false;
     }
     
-
-    public void searchNguyenLieuByMa(JTable table, String maNguyenLieu) {
+    public void searchNguyenLieuByTen(JTable table, String tenNguyenLieu) {
         if (table == null) {
             throw new IllegalArgumentException("Table is null");
         }
-        ArrayList<Object[]> data = nhapHangDAO.searchNguyenLieuByMa(maNguyenLieu);
+        ArrayList<Object[]> data = nhapHangDAO.searchNguyenLieuByTen(tenNguyenLieu);
         DefaultTableModel model = (DefaultTableModel) table.getModel();
         model.setRowCount(0); // Clear existing data
         for (Object[] row : data) {
             String maNguyenLieuResult = (String) row[0];
-            String tenNguyenLieu = (String) row[1];
+            String tenNguyenLieuResult = (String) row[1];
             double khoiLuong = (double) row[2];
             String trangThai = khoiLuong >= 1 ? "Còn hàng" : khoiLuong > 0.1 ? "Sắp hết" : "Hết hàng";
-            model.addRow(new Object[]{maNguyenLieuResult, tenNguyenLieu, khoiLuong, trangThai});
-
-  }
-}
+            model.addRow(new Object[]{maNguyenLieuResult, tenNguyenLieuResult, khoiLuong, trangThai});
+        }
+    }
     
     
 }
