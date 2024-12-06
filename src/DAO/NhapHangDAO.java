675da82dd34476b61a3cbe3642fbc20b690e9aa7
DinhThai04 <149181200+DinhThai04@users.noreply.github.com>
2024-12-06 15:15:04 +0700

    Update NhapHangDAO.java cập nhật

--- src/DAO/NhapHangDAO.java
+++ src/DAO/NhapHangDAO.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import javax.swing.table.DefaultTableModel;
+
 import Util.JDBCUtil;
 
 public class NhapHangDAO {
@@ -59,6 +61,41 @@ public class NhapHangDAO {
             e.printStackTrace();
         }
         return data;
+    }
+    public void updateNguyenLieu(DefaultTableModel model) throws SQLException {
+        try (Connection conn = JDBCUtil.getConnection()) {
+            PreparedStatement pstmt = null;
+            ResultSet rs = null;
+
+            for (int i = 0; i < model.getRowCount(); i++) {
+                String maNguyenLieu = (String) model.getValueAt(i, 0);
+                double khoiLuongNhap = (double) model.getValueAt(i, 2);
+                int donGiaMoi = (int) model.getValueAt(i, 3);
+
+                // Kiểm tra xem nguyên liệu đã tồn tại trong cơ sở dữ liệu hay chưa
+                String sqlCheck = "SELECT KhoiLuongNguyenLieu, DonGiaNguyenLieu FROM NguyenLieu WHERE MaNguyenLieu = ?";
+                pstmt = conn.prepareStatement(sqlCheck);
+                pstmt.setString(1, maNguyenLieu);
+                rs = pstmt.executeQuery();
+
+                if (rs.next()) {
+                    // Nguyên liệu đã tồn tại, cập nhật khối lượng
+                    double khoiLuongHienTai = rs.getDouble("KhoiLuongNguyenLieu");
+                    double khoiLuongMoi = khoiLuongHienTai + khoiLuongNhap;
+                    khoiLuongMoi = Math.round(khoiLuongMoi * 100.0) / 100.0;
+
+                    String sqlUpdate = "UPDATE NguyenLieu SET KhoiLuongNguyenLieu = ?, DonGiaNguyenLieu=? WHERE MaNguyenLieu = ?";
+                    pstmt = conn.prepareStatement(sqlUpdate);
+                    pstmt.setDouble(1, khoiLuongMoi);
+                    pstmt.setInt(2, donGiaMoi);
+                    pstmt.setString(3, maNguyenLieu);
+                    pstmt.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
           
 }
